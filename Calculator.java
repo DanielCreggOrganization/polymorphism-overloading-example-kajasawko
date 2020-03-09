01ddb79f8866f3c8260edaf925e6b89efde724af
kajasawko <kaja.sawko@gmail.com>
2020-03-09 17:36:53 +0000

    aaa

---
+++ Calculator.java
@@ -0,0 +1,38 @@
+
+public class Calculator {
+
+
+
+    public int add(int a, int b){
+        int sum;
+        sum = a + b;
+        System.out.println("Add method with 2 ints");
+        return sum;
+    }
+
+        public int add(int a, int b, int c){
+        int sum;
+        sum = a + b + c;
+        System.out.println("Add metod with 3 ints");
+        return sum;
+    }
+
+    public double subtract(double a, double b) {
+        double result;
+        result = a - b;
+        System.out.println("Subtract method with 2 doubles");
+        return result;
+        
+    }
+
+        public int subtract(int a, int b){
+        int result;
+        result = a - b;
+        System.out.println("Subtract method with 2 ints");
+        return result;
+        
+    }
+
+
+    
+}
---
+++ Main.java
@@ -0,0 +1,22 @@
+
+public class Main {
+    public static void main(String[] args) {
+        
+
+        Calculator calcObj1 = new Calculator();
+
+        
+        System.out.println(calcObj1.add(5, 10));
+
+     
+        System.out.println(calcObj1.add(1, 2, 3));
+
+        
+        System.out.println(calcObj1.subtract(8.8, 4.6));
+
+        
+        System.out.println(calcObj1.subtract(20, 10));
+    }
+
+    
+}
